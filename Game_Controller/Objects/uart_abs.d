1e4fda7460147c4aaa6acae74e6f79125aea7b8c
kierzek <kierzek@student.agh.edu.pl>
2022-12-19 17:34:21 +0100

    feat: Changed board to FRDM KL-05z

---
+++ Game_Controller/Objects/uart_abs.d
@@ -0,0 +1,12 @@
+.\objects\uart_abs.o: UART_abs.c
+.\objects\uart_abs.o: UART_abs.h
+.\objects\uart_abs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\uart_abs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\uart_abs.o: C:\Users\PKier\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
+.\objects\uart_abs.o: uart0.h
+.\objects\uart_abs.o: frdm_bsp.h
